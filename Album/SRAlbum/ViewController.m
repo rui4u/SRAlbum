d473b9f98310da8bda87fccc07f8b2d5320ac14a
sharui <sr_sharui@qq.com>
2018-07-17 16:46:59 +0800

    Update ViewController.m
    更新view显示

--- Album/SRAlbum/ViewController.m
+++ Album/SRAlbum/ViewController.m
@@ -63,13 +63,16 @@
 }
 
 - (void)setUpUI:(NSArray<SRImageInfoModel *>*) array{
-
+    for (UIView * subView in self.view.subviews) {
+        [subView removeFromSuperview];
+    }
+    
 	//      总列数
 	int totalColumns = 3;
-	
+	CGFloat width = ([UIScreen mainScreen].bounds.size.width )/ totalColumns - 10;
 	//       每一格的尺寸
-	CGFloat cellW = 50;
-	CGFloat cellH = 50;
+	CGFloat cellW = width;
+	CGFloat cellH = width;
 	
 	//    间隙
 	CGFloat margin =(self.view.frame.size.width - totalColumns * cellW) / (totalColumns + 1);
@@ -83,7 +86,7 @@
 		int col = index % totalColumns;
 		//根据行号和列号来确定 子控件的坐标
 		CGFloat cellX = margin + col * (cellW + margin);
-		CGFloat cellY = row * (cellH + margin);
+		CGFloat cellY = 100 + row * (cellH + margin);
 		cellView.frame = CGRectMake(cellX, cellY, cellW, cellH);
 		
 		// 添加到view 中
@@ -97,5 +100,4 @@
 	}
 	return _alertController;
 }
-
 @end
